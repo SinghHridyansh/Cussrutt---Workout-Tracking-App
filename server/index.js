ea3ef727d2d4c2669476b61b3420293be54877fc
SinghHridyansh <hridyansh.360@gmail.com>
2024-08-29 15:07:56 +0530

    Port Added

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -42,7 +43,7 @@ const connectDB = () => {
 const startServer = async () => {
   try {
     connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    app.listen(port, () => console.log(`Server started on port ${port}`));
   } catch (error) {
     console.log(error);
   }
